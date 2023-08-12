88c6c218d165bef14a6deb6e1cddd7943068db11
ㄴㄴ <>
2023-08-12 16:43:32 +0900

    fix: 업로드한 이미지 경로 찾는거 수정

--- backend/feedback/urls.py
+++ backend/feedback/urls.py
@@ -6,5 +6,5 @@ router = DefaultRouter()    # API의 URL 주소 자동 생성
 router.register(r'uploads', UserUploadViewSet)      # 어떤 모델과 뷰셋 연결할지 알려주는 역할
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('', include(router.urls)),     # 사용 가능한 API 엔드포인트 리스트만 보여줌
 ]
