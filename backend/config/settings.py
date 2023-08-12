e2ff329d1b6ccccc5da4da574c5dc72f160ebc43
ㄴㄴ <>
2023-08-12 16:53:48 +0900

    remove: 필요없는 폴더 삭제

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -44,8 +44,6 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "rest_framework.authtoken",
-    "image",
-    "language",
     "server",
     "users",
     "corsheaders",
