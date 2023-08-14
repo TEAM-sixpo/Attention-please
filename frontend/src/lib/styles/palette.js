49a7dfc6ac7d85d15cfc1c770e7fafc4fcbdfcc6
aetklimatcha <mpcovstraw@gmail.com>
2023-08-14 23:00:42 +0900

    feat: sign기능 추가

---
+++ frontend/src/lib/styles/palette.js
@@ -0,0 +1,11 @@
+const palette = {
+  PRIMARY: '#FFa973',
+  SUB: '#F8C56C',
+  DARKGRAY: '#404040',
+  GRAY: '#9d9d9d',
+  LIGHTGRAY: '#e4e4e4',
+  BACKGROUND: '#FAF8F1',
+  WHITE: '#fff',
+};
+
+export default palette;
