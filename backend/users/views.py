172b9379da04288eac3b91c3860d607f91c3484f
Lee YoungHo <bbchoi6@hufs.ac.kr>
2023-08-19 18:02:16 +0900

    rendering 문제 해결

--- backend/users/views.py
+++ backend/users/views.py
@@ -8,10 +8,19 @@ from rest_framework.authtoken.serializers import AuthTokenSerializer
 from .serializers import RegisterSerializer, LoginSerializer
 
 from rest_framework.decorators import api_view
+from django.shortcuts import render  # 이 부분을 추가해야 render 함수를 사용할 수 있습니다.
+
 
 class RegisterView(generics.CreateAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = RegisterSerializer
+
+    def get(self, request, *args, **kwargs):
+        return render(request, 'users/register.html')  # 회원가입 페이지를 보여줌
+
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
+
     
 class LoginView(generics.GenericAPIView):
     serializer_class = AuthTokenSerializer
